5c3621cc2acf0ef5313c7c7b024aaad0f319e474
beau-wilkins <88739590+beau-wilkins@users.noreply.github.com>
2021-12-12 16:46:14 -0500

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -65,14 +65,6 @@ def check_if_won():
         messagebox.showinfo("Tic Tac Toe", "X Wins!")
         disable_buttons()
 
-    elif b2["text"] == "x" and b5["text"] == "x" and b8["text"] == "x":
-        b2.config(bg="green")
-        b5.config(bg="green")
-        b8.config(bg="green")
-        winner = True
-        messagebox.showinfo("Tic Tac Toe", "X Wins!")
-        disable_buttons()
-
     elif b3["text"] == "x" and b6["text"] == "x" and b9["text"] == "x":
         b3.config(bg="green")
         b6.config(bg="green")
@@ -89,15 +81,15 @@ def check_if_won():
         messagebox.showinfo("Tic Tac Toe", "X Wins!")
         disable_buttons()
 
-#checks to see if o wins(computer)
-
-    elif b3["text"] == "o" and b5["text"] == "o" and b7["text"] == "o":
+    elif b3["text"] == "x" and b5["text"] == "x" and b7["text"] == "x":
         b3.config(bg="green")
         b5.config(bg="green")
         b7.config(bg="green")
         winner = True
-        messagebox.showinfo("Tic Tac Toe", "O Wins!")
+        messagebox.showinfo("Tic Tac Toe", "X Wins!")
         disable_buttons()
+
+#checks to see if o wins(computer)
 
     elif b1["text"] == "o" and b2["text"] == "o" and b3["text"] == "o":
         b1.config(bg="green")
@@ -139,14 +131,6 @@ def check_if_won():
         messagebox.showinfo("Tic Tac Toe", "O Wins!")
         disable_buttons()
 
-    elif b2["text"] == "o" and b5["text"] == "o" and b8["text"] == "o":
-        b2.config(bg="green")
-        b5.config(bg="green")
-        b8.config(bg="green")
-        winner = True
-        messagebox.showinfo("Tic Tac Toe", "O Wins!")
-        disable_buttons()
-
     elif b3["text"] == "o" and b6["text"] == "o" and b9["text"] == "o":
         b3.config(bg="green")
         b6.config(bg="green")
@@ -172,6 +156,11 @@ def check_if_won():
         disable_buttons()
 
 
+    #Check if tie
+    if count == 9 and winner == False:
+        messagebox.showinfo("Tic Tac Toe", "You Tied! Rest Game")
+        disable_buttons()
+
 #button clicked function
 def b_click(b):
     global clicked, count
@@ -189,28 +178,48 @@ def b_click(b):
     else:
         messagebox.showerror("Tic Tac Toe", "This box has already been chosen, please choose a different one!")
 
+#Resets the game
+def reset():
+    global b1, b2, b3, b4, b5, b6, b7, b8, b9
+    global clicked, count
+    clicked = True
+    count = 0
 #button build
-b1 = Button(root, text = "  ", font = ("Arial", 20), height = 3, width = 6,bg = "SystemButtonFace", command = lambda: b_click(b1))
-b2 = Button(root, text = "  ", font = ("Arial", 20), height = 3, width = 6,bg = "SystemButtonFace", command = lambda: b_click(b2))
-b3 = Button(root, text = "  ", font = ("Arial", 20), height = 3, width = 6,bg = "SystemButtonFace", command = lambda: b_click(b3))
+    b1 = Button(root, text = " ", font = ("Arial", 20), height = 3, width = 6,bg = "SystemButtonFace", command = lambda: b_click(b1))
+    b2 = Button(root, text = " ", font = ("Arial", 20), height = 3, width = 6,bg = "SystemButtonFace", command = lambda: b_click(b2))
+    b3 = Button(root, text = " ", font = ("Arial", 20), height = 3, width = 6,bg = "SystemButtonFace", command = lambda: b_click(b3))
 
-b4 = Button(root, text = "  ", font = ("Arial", 20), height = 3, width = 6,bg = "SystemButtonFace", command = lambda: b_click(b4))
-b5 = Button(root, text = "  ", font = ("Arial", 20), height = 3, width = 6,bg = "SystemButtonFace", command = lambda: b_click(b5))
-b6 = Button(root, text = "  ", font = ("Arial", 20), height = 3, width = 6,bg = "SystemButtonFace", command = lambda: b_click(b6))
+    b4 = Button(root, text = " ", font = ("Arial", 20), height = 3, width = 6,bg = "SystemButtonFace", command = lambda: b_click(b4))
+    b5 = Button(root, text = " ", font = ("Arial", 20), height = 3, width = 6,bg = "SystemButtonFace", command = lambda: b_click(b5))
+    b6 = Button(root, text = " ", font = ("Arial", 20), height = 3, width = 6,bg = "SystemButtonFace", command = lambda: b_click(b6))
 
-b7 = Button(root, text = "  ", font = ("Arial", 20), height = 3, width = 6,bg = "SystemButtonFace", command = lambda: b_click(b7))
-b8 = Button(root, text = "  ", font = ("Arial", 20), height = 3, width = 6,bg = "SystemButtonFace", command = lambda: b_click(b8))
-b9 = Button(root, text = "  ", font = ("Arial", 20), height = 3, width = 6,bg = "SystemButtonFace", command = lambda: b_click(b9))
+    b7 = Button(root, text = " ", font = ("Arial", 20), height = 3, width = 6,bg = "SystemButtonFace", command = lambda: b_click(b7))
+    b8 = Button(root, text = " ", font = ("Arial", 20), height = 3, width = 6,bg = "SystemButtonFace", command = lambda: b_click(b8))
+    b9 = Button(root, text = " ", font = ("Arial", 20), height = 3, width = 6,bg = "SystemButtonFace", command = lambda: b_click(b9))
 
-#button grid
-b1.grid(row = 0,column = 0)
-b2.grid(row = 0,column = 1)
-b3.grid(row = 0,column = 2)
+    #button grid
+    b1.grid(row = 0,column = 0)
+    b2.grid(row = 0,column = 1)
+    b3.grid(row = 0,column = 2)
 
-b4.grid(row = 1,column = 0)
-b5.grid(row = 1,column = 1)
-b6.grid(row = 1,column = 2)
+    b4.grid(row = 1,column = 0)
+    b5.grid(row = 1,column = 1)
+    b6.grid(row = 1,column = 2)
 
-b7.grid(row = 2,column = 0)
-b8.grid(row = 2,column = 1)
-b9.grid(row = 2,column = 2)
+    b7.grid(row = 2,column = 0)
+    b8.grid(row = 2,column = 1)
+    b9.grid(row = 2,column = 2)
+
+
+#Creates the menu
+my_menu = Menu(root)
+root.config(menu=my_menu)
+
+
+#creates options menu
+options_menu = Menu(my_menu, tearoff = False)
+my_menu.add_cascade(label="Options", menu=options_menu)
+options_menu.add_command(label="Reset Game",command=reset)
+
+reset()
+root.mainloop()
